90cc60abd7df82dfd70f6ed466a2f662723a3b6d
Joe Stanulis <info@stanulis.org>
2020-05-19 13:53:22 -0400

    Create O365-SSO.ps1

---
+++ O365-SSO.ps1
@@ -0,0 +1,22 @@
+# Your domain
+$dom = "example.org"
+# Entity ID
+$uri = ""
+# Duo Single Sign-On URL
+$url = ""
+# Duo Single Log-Out URL
+$logoutUrl = ""
+# Add certificate path from downloaded from Duo
+$cert=New-Object System.Security.Cryptography.X509Certificates.X509Certificate2("C:\Office 365.crt")
+$certData = [system.convert]::tobase64string($cert.rawdata)
+
+# Connect to O365 as global admin
+Connect-MsolService
+# Federate to Duo SSO
+Set-MsolDomainAuthentication –DomainName $dom -Authentication Federated -PassiveLogOnUri $url -IssuerUri $uri -LogOffUri $logoutUrl -PreferredAuthenticationProtocol SAMLP -SigningCertificate $certData
+
+# Confirm Federation
+Get-MSOLDomain -Domain $dom
+
+# Unfederate O365 from SSO
+# Set-MsolDomainAuthentication –DomainName $dom -Authentication Managed
